a05795f3e1824ccba8aca0092baf8df299125b29
Rizal Anggoro <gnoogler4@gmail.com>
2024-06-21 22:31:42 +0700

    refactor: remove unnecessary files

--- resources/js/Components/Breadcrumb.tsx
+++ resources/js/Components/Breadcrumb.tsx
@@ -1,49 +1,47 @@
 import { Link } from "@inertiajs/react";
 import {
-    Breadcrumb,
-    BreadcrumbItem,
-    BreadcrumbLink,
-    BreadcrumbList,
-    BreadcrumbPage,
-    BreadcrumbSeparator,
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
 } from "./ui/breadcrumb";
 
 type Props = {
-    items: {
-        title: string;
-        href?: string;
-    }[];
+  items: {
+    title: string;
+    href?: string;
+  }[];
 };
 
 export default function BreadcrumbComponent(props: Props) {
-    const items: React.ReactNode[] = [];
+  const items: React.ReactNode[] = [];
 
-    for (let a = 0; a < props.items.length; a++) {
-        const item = props.items[a];
-        items.push(
-            a < props.items.length - 1 ? (
-                <BreadcrumbItem key={"breadcrumb-item-" + a}>
-                    <BreadcrumbLink asChild>
-                        <Link href={item.href ?? "/"}>{item.title}</Link>
-                    </BreadcrumbLink>
-                </BreadcrumbItem>
-            ) : (
-                <BreadcrumbItem>
-                    <BreadcrumbPage>{item.title}</BreadcrumbPage>
-                </BreadcrumbItem>
-            )
-        );
-        if (a < props.items.length - 1)
-            items.push(
-                <BreadcrumbSeparator key={"breadcrumb-separator-" + a} />
-            );
-    }
-
-    return (
-        <>
-            <Breadcrumb>
-                <BreadcrumbList>{...items}</BreadcrumbList>
-            </Breadcrumb>
-        </>
+  for (let a = 0; a < props.items.length; a++) {
+    const item = props.items[a];
+    items.push(
+      a < props.items.length - 1 ? (
+        <BreadcrumbItem key={"breadcrumb-item-" + a}>
+          <BreadcrumbLink asChild>
+            <Link href={item.href ?? "/"}>{item.title}</Link>
+          </BreadcrumbLink>
+        </BreadcrumbItem>
+      ) : (
+        <BreadcrumbItem>
+          <BreadcrumbPage>{item.title}</BreadcrumbPage>
+        </BreadcrumbItem>
+      )
     );
+    if (a < props.items.length - 1)
+      items.push(<BreadcrumbSeparator key={"breadcrumb-separator-" + a} />);
+  }
+
+  return (
+    <>
+      <Breadcrumb>
+        <BreadcrumbList>{...items}</BreadcrumbList>
+      </Breadcrumb>
+    </>
+  );
 }
