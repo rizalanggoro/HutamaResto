b674ed7334705c5feddc242486a35250b9b2404f
Rizal Anggoro <gnoogler4@gmail.com>
2024-07-03 08:41:24 +0700

    update seeder

--- database/seeders/CustomerOrdersSeeder.php
+++ database/seeders/CustomerOrdersSeeder.php
@@ -17,50 +17,53 @@ class CustomerOrdersSeeder extends Seeder {
      */
     public function run(): void {
         $customers = User::whereRole('customer')->get();
-        $franchise = Franchise::first();
-        $menus = $franchise->menus()->get();
+        $franchises = Franchise::all();
 
         foreach ($customers as $customer) {
-            for ($a = 0; $a < 10; $a++) {
-                $order = Order::create([
-                    'user_id' => $customer->id,
-                    'franchise_id' => $franchise->id,
-                    'status' => $a % 2 === 0 ? 'waiting_payment' : 'waiting_payment_verification',
-                    'receipt_path' => $a % 2 === 0 ? null : 'seeder/receipts/buktitransfer.jpg',
-                ]);
+            foreach ($franchises as $franchise) {
+                $menus = $franchise->menus()->get();
 
-                for ($b = 0; $b < rand(1, count($menus)); $b++) {
-                    OrderItem::create([
-                        'order_id' => $order->id,
-                        'menu_id' => $menus[$b]->id,
-                        'count' => rand(1, 5),
-                    ]);
-                }
-            }
-
-            for ($a = 0; $a < 7; $a++) {
-                $order = Order::create([
-                    'user_id' => $customer->id,
-                    'franchise_id' => $franchise->id,
-                    'status' => 'done',
-                ]);
-
-                for ($b = 0; $b < rand(1, count($menus)); $b++) {
-                    OrderItem::create([
-                        'order_id' => $order->id,
-                        'menu_id' => $menus[$b]->id,
-                        'count' => rand(1, 5),
-                    ]);
-                }
-
-                if (rand(1, 2) === 2) {
-                    Review::create([
-                        'star' => rand(1, 5),
-                        'review' => fake()->sentence(),
+                for ($a = 0; $a < 10; $a++) {
+                    $order = Order::create([
                         'user_id' => $customer->id,
-                        'order_id' => $order->id,
                         'franchise_id' => $franchise->id,
+                        'status' => $a % 2 === 0 ? 'waiting_payment' : 'waiting_payment_verification',
+                        'receipt_path' => $a % 2 === 0 ? null : 'seeder/receipts/buktitransfer.jpg',
                     ]);
+
+                    for ($b = 0; $b < rand(1, count($menus)); $b++) {
+                        OrderItem::create([
+                            'order_id' => $order->id,
+                            'menu_id' => $menus[$b]->id,
+                            'count' => rand(1, 5),
+                        ]);
+                    }
+                }
+
+                for ($a = 0; $a < 7; $a++) {
+                    $order = Order::create([
+                        'user_id' => $customer->id,
+                        'franchise_id' => $franchise->id,
+                        'status' => 'done',
+                    ]);
+
+                    for ($b = 0; $b < rand(1, count($menus)); $b++) {
+                        OrderItem::create([
+                            'order_id' => $order->id,
+                            'menu_id' => $menus[$b]->id,
+                            'count' => rand(1, 5),
+                        ]);
+                    }
+
+                    if (rand(1, 2) === 2) {
+                        Review::create([
+                            'star' => rand(1, 5),
+                            'review' => fake()->sentence(),
+                            'user_id' => $customer->id,
+                            'order_id' => $order->id,
+                            'franchise_id' => $franchise->id,
+                        ]);
+                    }
                 }
             }
         }
